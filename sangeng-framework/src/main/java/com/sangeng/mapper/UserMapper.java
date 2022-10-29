42eaf341a3e226d6352815e23f7628bf22765713
AlenXia <wanjunxingbei@qq.com>
2022-10-29 16:58:40 +0800

    study

---
+++ sangeng-framework/src/main/java/com/sangeng/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.sangeng.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sangeng.domain.entity.User;
+
+/**
+ * 用户表(User)表数据库访问层
+ *
+ * @author makejava
+ * @since 2022-10-29 16:53:21
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
