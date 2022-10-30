291240ff8be546f5cd6ba3f1354ffdf6e15e9a02
AlenXia <wanjunxingbei@qq.com>
2022-10-30 11:01:06 +0800

    study

--- sangeng-blog/src/main/java/com/sangeng/controller/BlogLoginController.java
+++ sangeng-blog/src/main/java/com/sangeng/controller/BlogLoginController.java
@@ -2,8 +2,11 @@ package com.sangeng.controller;
 
 import com.sangeng.domain.ResponseResult;
 import com.sangeng.domain.entity.User;
+import com.sangeng.enums.AppHttpCodeEnum;
+import com.sangeng.exception.SystemException;
 import com.sangeng.service.BlogLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,10 @@ public class BlogLoginController {
 
     @PostMapping("/login")
     public ResponseResult login(@RequestBody User user) {
+        if (!StringUtils.hasText(user.getUserName())) {
+            // 提示必须要传用户名
+            throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
+        }
         return blogLoginService.login(user);
     }
 }
