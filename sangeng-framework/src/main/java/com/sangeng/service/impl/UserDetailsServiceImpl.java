4a14aa555e57798572cab207218f1ba27b0baf1b
AlenXia <wanjunxingbei@qq.com>
2022-10-31 09:36:28 +0800

    study

--- sangeng-framework/src/main/java/com/sangeng/service/impl/UserDetailsServiceImpl.java
+++ sangeng-framework/src/main/java/com/sangeng/service/impl/UserDetailsServiceImpl.java
@@ -5,7 +5,6 @@ import com.sangeng.domain.entity.LoginUser;
 import com.sangeng.domain.entity.User;
 import com.sangeng.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -27,12 +26,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // 根据用户名查询用户条件
-        LambdaQueryWrapper<User> queryWrapper=new LambdaQueryWrapper();
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper();
         queryWrapper.eq(User::getUserName, username);
         User user = userMapper.selectOne(queryWrapper);
 
         // 判断是否查到用户，如果没查到则抛出异常
-        if(Objects.isNull(user)){
+        if (Objects.isNull(user)) {
             throw new RuntimeException("用户不存在");
         }
 
